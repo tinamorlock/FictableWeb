dd2a448cb853bfbfe982eada06d63eada8692615
tinamorlock <tinamorlock@gmail.com>
2023-07-09 10:01:32 -0500

    Extended user model and schema. Added book model.

--- app/models.py
+++ app/models.py
@@ -25,11 +25,48 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
+    username = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    bio = Column(String, nullable=True)
+    website = Column(String, nullable=True)
+    facebook_link = Column(String, nullable=True)
+    twitter_link = Column(String, nullable=True)
+    ig_link = Column(String, nullable=True)
+    tiktok_link = Column(String, nullable=True)
+    amazon_link = Column(String, nullable=True)
+    goodreads_link = Column(String, nullable=True)
+    bookbub_link = Column(String, nullable=True)
+    is_author = Column(Boolean, nullable=False, server_default='False')
+    genre = Column(String, nullable=True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
+
+# like system for social posts
 
 class Vote(Base):
     __tablename__ = "votes"
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+
+
+# the book model will allow authors to list their books on their profile
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    series_name = Column(String, nullable=True)
+    series_number = Column(Integer, nullable=True)
+    published_year = Column(Integer, nullable=False)
+    genre = Column(String, nullable=False)
+    subgenre = Column(String, nullable=False)
+    ebook_price = Column(String, nullable=True)
+    paperback_price = Column(String, nullable=True)
+    hardcover_price = Column(String, nullable=True)
+    audiobook_price = Column(String, nullable=True)
+    description = Column(String, nullable=False)
+    amazon_link = Column(String, nullable=False)
+    goodreads_link = Column(String, nullable=False)
+    bookbub_link = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
--- app/schemas.py
+++ app/schemas.py
@@ -10,8 +10,21 @@ class UserCreate(BaseModel):
 
 class UserOut(BaseModel):
     id: int
+    username: str
+    is_author: bool = False
     email: EmailStr
     created_at: datetime
+    bio: Optional[str] = None
+    website: Optional[str] = None
+    facebook_link: Optional[str] = None
+    twitter_link: Optional[str] = None
+    ig_link: Optional[str] = None
+    tiktok_link: Optional[str] = None
+    amazon_link: Optional[str] = None
+    goodreads_link: Optional[str] = None
+    bookbub_link: Optional[str] = None
+    genre: Optional[str] = None
+
 
     class Config:
         orm_mode=True
