34709ef8a4e578807dab48847c855d3f4144bb4f
tinamorlock <tinamorlock@gmail.com>
2023-07-13 11:14:23 -0500

    book/update route added and tested working.

--- app/routers/book.py
+++ app/routers/book.py
@@ -32,3 +32,17 @@ def create_book(book: schemas.BookCreate, db: Session = Depends(get_db), current
     db.commit()
     db.refresh(new_book)
     return new_book
+
+# updates a book
+
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Book)
+def update_book(id: int, book: schemas.BookCreate, db: Session = Depends(get_db), current_user: schemas.UserOut = Depends(oauth2.get_current_user)):
+    updated_book = db.query(models.Book).filter(models.Book.id == id)
+    books = updated_book.first()
+    if books == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"book with id: {id} does not exist")
+    if books.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorized to update this book.")
+    updated_book.update(book.dict(), synchronize_session=False)
+    db.commit()
+    return updated_book.first()
