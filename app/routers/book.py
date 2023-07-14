fc8b5a7b9f27c341a5578ca76b0a159fb560f503
tinamorlock <tinamorlock@gmail.com>
2023-07-14 07:32:00 -0500

    delete/book tested working.

--- app/routers/book.py
+++ app/routers/book.py
@@ -46,3 +46,16 @@ def update_book(id: int, book: schemas.BookCreate, db: Session = Depends(get_db)
     updated_book.update(book.dict(), synchronize_session=False)
     db.commit()
     return updated_book.first()
+
+# deletes a book
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_book(id: int, db: Session = Depends(get_db), current_user: schemas.UserOut = Depends(oauth2.get_current_user)):
+    book = db.query(models.Book).filter(models.Book.id == id)
+    if book.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"book with id: {id} does not exist")
+    if book.first().owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authorized to delete this book.")
+    book.delete(synchronize_session=False)
+    db.commit()
+    return f"Book with id: {id} deleted."
